ffa12ac627d85871c0ff4f1c3c232b2028f07c02
Gurwi <gs3335769@gmail.com>
2024-07-16 21:33:26 +0200

    a

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -1,50 +1,37 @@
-'use client'
 import { FaTelegram } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
 import { SiSpigotmc } from "react-icons/si";
 import NavBarContactButton from "./contactbutton";
 import { useEffect, useState } from "react";
 
-const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-const [isMobile, setMobile] = useState(false);
+// const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+// const [screenType, setScreenType] = useState("Desktop");
 
-useEffect(() => {
-    if (screenWidth >= 770) {
-      setMobile(false)
-    } else {
-      setMobile(true)
-    }
-  }, [screenWidth])
+// useEffect(() => {
+//     if (screenWidth >= 770) {
+//       setScreenType("Desktop")
+//     } else {
+//       setScreenType("Mobile")
+//     }
+//   }, [screenWidth])
 
 export default function NavBar() {
 
     return (
-        <>
-            {
-                
-                (isMobile) ? 
+        <nav className="not-text py-14 grid grid-cols-2 sticky">
+            <ul className="font-martian text-sm opacity-15 font-semibold list-none space-x-10 justify-start sm:flex grid items-center">
+                <li><a href='#home'>&lt;home&gt;</a></li>
+                <li><a href='#experties'>&lt;experties&gt;</a></li>
+                <li><a href='#projects'>&lt;projects&gt;</a></li>
+            </ul>
 
-                <h1>Mobile</h1> 
-
-                : 
-
-                <nav className="not-text py-14 grid grid-cols-2 w-full sticky">
-                    <ul className="font-martian text-sm opacity-15 font-semibold list-none space-x-10 justify-start flex">
-                        <li><a href='#home'>&lt;home&gt;</a></li>
-                        <li><a href='#experties'>&lt;experties&gt;</a></li>
-                        <li><a href='#projects'>&lt;projects&gt;</a></li>
-                    </ul>
-        
-                    <ul className="font-martian text-sm font-semibold list-none space-x-10 justify-end flex">
-                        <li><a href='https://github.com/Gurwi30' target="_blank"><FaGithub className='icon' size={20}></FaGithub></a></li>
-                        <li><a href='https://t.me/Gurwi30' target="_blank"><FaTelegram className='icon' size={20}></FaTelegram></a></li>
-                        <li><a href='https://www.spigotmc.org/members/gurwinder.717908/' target="_blank"><SiSpigotmc className='icon' size={20}></SiSpigotmc></a></li>
-                        <li><NavBarContactButton /></li>
-                    </ul>
-                </nav>
-                
-            }
-        </>
+            <ul className="font-martian text-sm font-semibold list-none space-x-10 justify-end flex">
+                <li><a href='https://github.com/Gurwi30' target="_blank"><FaGithub className='icon' size={20}></FaGithub></a></li>
+                <li><a href='https://t.me/Gurwi30' target="_blank"><FaTelegram className='icon' size={20}></FaTelegram></a></li>
+                <li><a href='https://www.spigotmc.org/members/gurwinder.717908/' target="_blank"><SiSpigotmc className='icon' size={20}></SiSpigotmc></a></li>
+                <li><NavBarContactButton /></li>
+            </ul>
+        </nav>
     )
 
 }
